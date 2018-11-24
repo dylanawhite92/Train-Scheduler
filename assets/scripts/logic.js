334344731a634a7d1098239bec8e8be36865c00d
Narwhal Nancy <dylanawhite11@gmail.com>
2018-11-24 14:11:01 -0600

    new files created

---
+++ assets/scripts/logic.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+
+var config = {
+    apiKey: "AIzaSyB3BqY6X1mxOprZPSTwZuwjkXtZBgcOtFs",
+    authDomain: "homework-7-6bfb8.firebaseapp.com",
+    databaseURL: "https://homework-7-6bfb8.firebaseio.com",
+    projectId: "homework-7-6bfb8",
+    storageBucket: "",
+    messagingSenderId: "344706120446"
+  };
+
+firebase.initializeApp(config);
